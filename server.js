a9249311d86df2dd250f17d7b794cfa67a74758e
Maja <maja.ajhorn@gmail.com>
2025-04-13 14:30:22 +0200

    fixing deployment

--- server.js
+++ server.js
@@ -14,10 +14,22 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://carematch.vercel.app'
+];
+
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://carematch.vercel.app'],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
